362edc4bf7b5b7c0ab6cce35cb8d5eee65023bf8
thefitdevproject <thefitdevproject@gmail.com>
2021-01-31 16:36:46 -0500

    Fit Dev Project Initial Commit of fitdevproject code

---
+++ FitDevProject/src/main/java/com/fitdevproject/FitDevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fitdevproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitDevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitDevProjectApplication.class, args);
+	}
+
+}
---
+++ FitDevProject/src/test/java/com/fitdevproject/FitDevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitdevproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitDevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
